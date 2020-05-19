02465c9c32328c07549d63c36fab58fa369f195c
Henrik J <henke.asaka@gmail.com>
2020-05-19 12:52:55 +0200

    eyeyryt

--- iMat/iMat/src/iMat/Categories/CategoriesController.java
+++ iMat/iMat/src/iMat/Categories/CategoriesController.java
@@ -1,13 +1,8 @@
 package iMat.Categories;
 
-import iMat.MainController;
-import iMat.ProductCard;
 import javafx.fxml.FXML;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Pane;
 import se.chalmers.cse.dat216.project.*;
 
-import java.awt.*;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,6 +10,7 @@ import java.util.stream.Stream;
 
 public class CategoriesController {
 
+<<<<<<< HEAD
     @FXML
     FlowPane productFlowPane;
     @FXML
@@ -28,6 +24,13 @@ public class CategoriesController {
     public List<Product> dryckList = Stream.concat(idh.getProducts(ProductCategory.COLD_DRINKS).stream(), idh.getProducts(ProductCategory.HOT_DRINKS).stream()).collect(Collectors.toList());
 
     public List<Product> fruktBärList = Stream.of(idh.getProducts(ProductCategory.MELONS), idh.getProducts(ProductCategory.EXOTIC_FRUIT),
+=======
+    IMatDataHandler idh = IMatDataHandler.getInstance();
+
+    List<Product> dryckList = Stream.concat(idh.getProducts(ProductCategory.COLD_DRINKS).stream(), idh.getProducts(ProductCategory.HOT_DRINKS).stream()).collect(Collectors.toList());
+
+    List<Product> fruktBärList = Stream.of(idh.getProducts(ProductCategory.MELONS), idh.getProducts(ProductCategory.EXOTIC_FRUIT),
+>>>>>>> parent of f0628c7... tog bort categoryscreen och fixade så att kategoriknapparna går att klicka på
             idh.getProducts(ProductCategory.CITRUS_FRUIT), idh.getProducts(ProductCategory.BERRY)).flatMap(Collection::stream).collect(Collectors.toList());
 
     public List<Product> grönsakerList = Stream.of(idh.getProducts(ProductCategory.VEGETABLE_FRUIT), idh.getProducts(ProductCategory.ROOT_VEGETABLE),
@@ -42,6 +45,7 @@ public class CategoriesController {
     public List<Product> skafferiList = Stream.of(idh.getProducts(ProductCategory.FLOUR_SUGAR_SALT), idh.getProducts(ProductCategory.HERB),
             idh.getProducts(ProductCategory.PASTA), idh.getProducts(ProductCategory.BREAD)).flatMap(Collection::stream).collect(Collectors.toList());
 
+<<<<<<< HEAD
     public List<Product> sötsakerSnacksList = Stream.concat(idh.getProducts(ProductCategory.SWEET).stream(), idh.getProducts(ProductCategory.NUTS_AND_SEEDS).stream()).collect(Collectors.toList());
 
 
@@ -52,4 +56,49 @@ public class CategoriesController {
             productFlowPane.getChildren().add(new ProductCard(p, ProductCard.cardType.category));
         }
     }
+=======
+    List<Product> sötsakerSnacksList = Stream.concat(idh.getProducts(ProductCategory.SWEET).stream(), idh.getProducts(ProductCategory.NUTS_AND_SEEDS).stream()).collect(Collectors.toList());
+
+
+    @FXML
+    void onClickDRYCK(){
+        /*POPULATECATEGORYSCREEN(dryckList, "Dryck");*/
+    }
+
+    @FXML
+    void onClickFRUKTBÄR(){
+        /*POPULATECATEGORYSCREEN(fruktBärList, "Frukt & bär");*/
+    }
+
+    @FXML
+    void onClickGRÖNSAKER(){
+        /*POPULATECATEGORYSCREEN(grönsakerList, "Grönsaker");*/
+    }
+
+    @FXML
+    void onClickKÖTTFISK(){
+        /*POPULATECATEGORYSCREEN(dryckList, "Dryck");*/
+    }
+
+    @FXML
+    void onClickMEJERI(){
+        /*POPULATECATEGORYSCREEN(mejeriList, "Mejeri");*/
+    }
+
+    @FXML
+    void onClickPOTATISRIS(){
+        /*POPULATECATEGORYSCREEN(potatisRisList, "Potatis & ris");*/
+    }
+
+    @FXML
+    void onClickSKAFFERI(){
+        /*POPULATECATEGORYSCREEN(skafferList, "Skafferi");*/
+    }
+
+    @FXML
+    void onClickSÖTSAKERSNACKS(){
+        /*POPULATECATEGORYSCREEN(sötsakerSnacksList, "Sötsaker & snacks");*/
+    }
+
+>>>>>>> parent of f0628c7... tog bort categoryscreen och fixade så att kategoriknapparna går att klicka på
 }
