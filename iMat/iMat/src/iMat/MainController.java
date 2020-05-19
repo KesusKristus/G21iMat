bd69dfc480da4d9173438c2f78129e0c4b6371ca
KesusKristus <57448724+KesusKristus@users.noreply.github.com>
2020-05-19 11:39:33 +0200

    .

--- iMat/iMat/src/iMat/MainController.java
+++ iMat/iMat/src/iMat/MainController.java
@@ -4,11 +4,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
+    private IMatDataHandler dataHandler;
+    private CategoryScreen categoryScreen;
 
     @FXML AnchorPane startScreen;
     @FXML ScrollPane shoppingCartPane;
@@ -21,5 +25,9 @@ public class MainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public IMatDataHandler getDataHandler() {
+        return dataHandler;
     }
 }
